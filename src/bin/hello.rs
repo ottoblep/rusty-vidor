8a89704c8dceec705f9770850eecd7f129b1a85e
Severin Lochschmidt <seviron53@gmail.com>
2026-05-14 18:27:30 +0200

    Generate cortex-m quickstart

---
+++ src/bin/hello.rs
@@ -0,0 +1,11 @@
+#![no_main]
+#![no_std]
+
+use rusty_vidor as _; // global logger + panicking-behavior + memory layout
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    defmt::println!("Hello, world!");
+
+    rusty_vidor::exit()
+}
