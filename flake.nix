46709fc7930c12221f780bc66c452b1536acf906
Severin Lochschmidt <seviron53@gmail.com>
2026-05-14 18:22:26 +0200

    Add devshell

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Embedded rust devenv";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            rust-analyzer
+            probe-rs-tools
+            (rust-bin.nightly.latest.default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "thumbv6m-none-eabi" ]; #"arm-unknown-linux-gnueabihf" ];
+            })
+          ];
+
+          shellHook = ''
+            export PATH="$PATH:~/.cargo/bin"
+          '';
+        };
+      }
+    );
+}
