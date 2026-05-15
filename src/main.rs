b8ed9b0b1a7770301aefb2e0bb036c6f854f6d71
Severin Lochschmidt <seviron53@gmail.com>
2026-05-16 00:41:59 +0200

    Format

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@
 use arduino_mkrvidor4000 as bsp;
 use bsp::hal;
 
-use panic_halt as _;
 use bsp::entry;
-use hal::clock::{GenericClockController, ClockGenId, ClockSource};
+use hal::clock::{ClockGenId, ClockSource, GenericClockController};
 use hal::delay::Delay;
 use hal::pac::{CorePeripherals, Peripherals};
 use hal::prelude::*;
+use panic_halt as _;
 
+use hal::sercom::v2::uart::{BaudMode, BitOrder, EightBit, Oversampling, Pads, StopBits};
 use hal::sercom::v2::{Sercom5, uart};
-use hal::sercom::v2::uart::{BaudMode, BitOrder, Oversampling, Pads, StopBits, EightBit};
 
 #[entry]
 fn main() -> ! {
@@ -36,9 +36,7 @@ fn main() -> ! {
     let mut delay = Delay::new(core.SYST, &mut clocks);
 
     // UART
-    let pads = Pads::<Sercom5>::default()
-        .rx(pins.rx)
-        .tx(pins.tx);
+    let pads = Pads::<Sercom5>::default().rx(pins.rx).tx(pins.tx);
     let mut uart = uart::Config::new(&(peripherals.PM), peripherals.SERCOM5, pads, 48.mhz())
         .baud(115200.hz(), BaudMode::Arithmetic(Oversampling::Bits8))
         .char_size::<EightBit>()
