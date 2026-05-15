3000c483fc82d0b44c65b3072280bd8fc6dd7964
Severin Lochschmidt <seviron53@gmail.com>
2026-05-16 00:30:55 +0200

    Configure UART Clock

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use arduino_mkrvidor4000::clock::{ClockGenId, ClockSource};
 use arduino_mkrvidor4000 as bsp;
 use bsp::hal;
 
@@ -25,6 +26,14 @@ fn main() -> ! {
         &mut peripherals.NVMCTRL,
     );
     let mut pins = bsp::Pins::new(peripherals.PORT);
+
+    // UART Clock
+    clocks.configure_gclk_divider_and_source(ClockGenId::GCLK0, 1, ClockSource::XOSC32K, false);
+    clocks.configure_standby(ClockGenId::GCLK0, true);
+    let gclk = clocks.get_gclk(ClockGenId::GCLK0).unwrap();
+    clocks.sercom5_core(&gclk);
+
+    // GPIO
     let mut led = pins.led_builtin.into_open_drain_output(&mut pins.port);
     let mut delay = Delay::new(core.SYST, &mut clocks);
 
@@ -32,17 +41,18 @@ fn main() -> ! {
     let pads = Pads::<Sercom5>::default()
         .rx(pins.rx)
         .tx(pins.tx);
-    let mut uart = uart::Config::new(&(peripherals.PM), peripherals.SERCOM5, pads, 10.mhz())
-        .baud(1.mhz(), BaudMode::Arithmetic(Oversampling::Bits16))
+    let uart = uart::Config::new(&(peripherals.PM), peripherals.SERCOM5, pads, 48.mhz())
+        .baud(115200.hz(), BaudMode::Arithmetic(Oversampling::Bits8))
         .char_size::<NineBit>()
         .bit_order(BitOrder::LsbFirst)
         .stop_bits(StopBits::OneBit)
         .enable();
 
+
     loop {
         delay.delay_ms(200u8);
-        let data: u16 = 0xABCD;
-        let _ = uart.write(data);
+        // let data: u16 = 0xABCD;
+        // let _ = uart.write(data);
         led.set_high().unwrap();
         delay.delay_ms(200u8);
         led.set_low().unwrap();
